9ed813e0e2d9ed83f5e4cd9143d937e4f82cef2f
lawal <sphade010@gmail.com>
2024-10-17 14:15:13 +0100

    asdc asd

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -2,6 +2,7 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { todoTable, userTable } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
+import { dev } from '$app/environment';
 
 export const load = (async ({ locals: { db, user } }) => {
 	if (!user) redirect(308, '/login');
@@ -23,12 +24,18 @@ export const actions = {
 		const form = await request.formData();
 		const content = form.get('content') as string;
 		const file = form.get('file') as File;
+		console.log('🚀 ~ default: ~ file:', file);
 		if (!user) redirect(308, '/login');
-		await bucket.put(file.name, file);
+		const object = await bucket.put(file.name, file);
+		const base = dev
+			? 'http://127.0.0.1:8788/'
+			: 'https://pub-23cce33cce57409880410f1196c3b258.r2.dev/';
+		// const imageUrl = URL.createObjectURL(object);
+		// console.log('🚀 ~ default: ~ imageUrl:', imageUrl);
 		await db.insert(todoTable).values({
 			userId: user.id,
 			content,
-			media: `https://pub-23cce33cce57409880410f1196c3b258.r2.dev/${file.name}`
+			media: `${base}${file.name}`
 		});
 	}
 };
